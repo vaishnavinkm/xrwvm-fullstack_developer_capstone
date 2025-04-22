ea0a531df1a57142cb3efa9a36e4a60e3f5bb82b
vaishnavinkm <vaishnavinikam1999@gmail.com>
2025-04-22 09:30:11 -0400

    Updated app.js in database

--- server/database/app.js
+++ server/database/app.js
@@ -58,17 +58,36 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+    try {
+      const dealers = await Dealerships.find();
+      res.json(dealers)
+    } catch (error){
+      res.status(500).json({error: 'Error fetching dealerships'});
+    }
 });
-
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+   try{
+    const state = req.params.state;
+    const dealer = await Dealerships.find({state});
+    res.json(dealer);
+   } catch (error){
+    res.status(500).json({error: 'Error fetching Dealerships by state'});
+   }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+   try{
+    const id = parseInt(req.params.id);
+    const dealer =await Dealerships.find({id});
+    if (!dealer){
+        return res.status(404).json({erro: 'Dealer not found'});
+    }
+    res.json(dealer);
+   }catch (error){
+    res.status(500).json({error: 'Error fetching dealership by ID'});
+   }
 });
 
 //Express route to insert review
