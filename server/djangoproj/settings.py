17a92356300971848f53deb141c651011e94cf92
vaishnavinkm <vaishnavinikam1999@gmail.com>
2025-04-30 15:32:57 -0400

    admin

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,11 +31,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
-    "https://vaishnavinik-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01."
+    "https://vaishnavinik-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01."
     "proxy.cognitiveclass.ai",
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "https://vaishnavinik-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01."
+    "https://vaishnavinik-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01."
     "proxy.cognitiveclass.ai",
 ]
 
